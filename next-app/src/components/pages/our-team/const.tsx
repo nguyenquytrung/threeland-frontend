9bd81dd87d79ce205dbb2adce25d713a149fc8ab
dungnn <tienphongcutin@gmail.com>
2024-06-12 15:50:18 +0700

    upcode

---
+++ next-app/src/components/pages/our-team/const.tsx
@@ -0,0 +1,49 @@
+import ourTeam_01 from '@/assets/images/ourteam/ourteam_01.png';
+import ourTeam_02 from '@/assets/images/ourteam/ourteam_02.png';
+import ourTeam_03 from '@/assets/images/ourteam/ourteam_03.png';
+import ourTeam_04 from '@/assets/images/ourteam/ourteam_04.png';
+
+export const OurTeamActivitiesList = [
+    {
+      id: 1,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_01,
+    },
+    {
+      id: 2,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_02,
+    },
+    {
+      id: 3,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_03,
+    },
+    {
+      id: 4,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_04,
+    },
+    {
+      id: 5,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_01,
+    },
+    {
+      id: 6,
+      content: 'Summer means happy times and good sunshine. With our annually summer vacation, it means going to the beach, having fun and creating wonderful memories together.',
+      title: "Threeland summer vacation",
+      time: 'June 28, 2018',
+      image: ourTeam_02,
+    },
+  ];
